612fb94b1b1258d44645d08e4e0d015f3230ccde
waihpone <whharry7@gmail.com>
2025-05-27 22:21:37 +0800

    fixed auth layout for payment

--- backend/resources/js/Layouts/AuthenticatedLayout.jsx
+++ backend/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -98,11 +98,13 @@ export default function AuthenticatedLayout({ header, children, showBackButton =
                                 </NavLink>
                             </>
                         )}
-                        <NavLink
-                            href={route('payments.index')} active={route().current('payments.index')}
-                        >
-                            Payment
-                        </NavLink>
+                        {(role === 'admin') && (
+                            <NavLink
+                                href={route('payments.index')} active={route().current('payments.index')}
+                            >
+                                Payment
+                            </NavLink>
+                        )}
                         <NavLink
                             href={route('map.parkmap')}
                             active={route().current('map.parkmap')}
